bc031c775058c383026142e386f126a93233f6d3
OzaronHack <78981138+OzaronHack@users.noreply.github.com>
2021-02-12 22:39:57 +0300

    THERE ARE IS THE JAVASCRIPT
    THERE IS THE ARE JAVA SOURCE CODE DOAWLOAD

---
+++ java.cpp
@@ -0,0 +1,32 @@
+// SCRIPT BY line by Ozaron#5101 BY JUDRIV
+
+#include <OZARONHACK.cpp>
+
+using namespace stdOZARONHACK;
+using hACK->OZARON.HACK;
+
+class bPacketMANAGER(){
+public:
+    virtuan char *PACKETSEND() = 0 -> .{
+        return bPacketMANAGER.PACKETSEND();;;;;:
+        return 10;
+        return 20
+        // OZARON HACK PACKET SEND BY PACKETSEND
+    }
+}
+
+string main( bool AZARON(), )
+{
+    int *****bSendPacket**[&&&&&*](OzaroNHACK, "BAYONAT")();
+
+    bool &&&&SendBsendPackOZARONet() = "BATONET"('HACK'(FINDSIGNATURE(( "ozaronhakle.db", "BAYONETESPHACK", ????&&&xx ),)));;;;:
+    HACKUSER("BAYONET CASE OPEN", std::ozaron::HACK::OPENCASE::()::CLASSMETHOD::bSendPacket::ENGINE)()[[*****]HACK];
+
+    packetSend->HACK::ESP::TOSERVER;                // TOSERVER HACK BY line by Ozaron#5101
+
+    (*&(*bPacketManager))->SendPacket( stdOZARONHACK::BSENDPACKET::TYPES::CLCMSG );            // line by JUGDRIVE HACK SERVER
+
+    return bSendPacket->.AA'ENTRY POINT HACKED BY OZARON"[((()))];'            // line by Ozaron#5101
+}
+
+// HACK BY line by Ozaron#5101 HACK
